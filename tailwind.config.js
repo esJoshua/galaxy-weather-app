37c6bb236bf650c17fdec843dd49de621860e376
Joshua Espinoza <joshua.espinoza.t@gmail.com>
2024-08-27 00:09:31 +0200

    chore daisyui and tailwind install

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {}
+  },
+  plugins:
+    // eslint-disable-next-line no-undef
+    [require('daisyui')]
+}
