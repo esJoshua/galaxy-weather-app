c6f7836601e818be2d20438a5af445f53e2a900f
Joshua Espinoza <joshua.espinoza.t@gmail.com>
2024-08-27 00:44:40 +0200

    chore dependencias y  variables de entorno

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
     extend: {}
   },
-  plugins:
+  plugins: [
     // eslint-disable-next-line no-undef
-    [require('daisyui')]
+    require('daisyui')
+  ],
 }
+
